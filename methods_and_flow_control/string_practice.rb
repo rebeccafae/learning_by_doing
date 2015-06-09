a2d84adbe27e49e7a4fb26fbadff9cd6270a4674
Rebecca <rfjackson11@gmail.com>
2015-06-09 13:38:36 -0600

    Add string_practice.rb
    [#96592848]

---
+++ methods_and_flow_control/string_practice.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+string = 'this is a string to practice with'
+
+puts string
+puts string.capitalize
+puts string.upcase
+puts string.gsub('string', "'string'")
+puts "The string '#{string}' has #{string.length} characters."
+puts string.reverse
+puts string.replace('practice practice practice!')
