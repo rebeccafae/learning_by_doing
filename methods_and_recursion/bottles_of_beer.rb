83f44417cc83ca2a318aeaec1a45f9b8ebccf155
Rebecca <rfjackson11@gmail.com>
2015-06-11 16:10:19 -0600

    Make methods shorter by adding more methods
    [#96593178]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -10,18 +10,21 @@ def pluralize(number, string)
   end
 end
 
+def ending
+  puts 'No more bottles of beer on the wall, no more bottles of beer.'
+  puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+end
+
 def song(num_bottles)
   if num_bottles > 0
     print "#{pluralize(num_bottles, 'bottle')} of beer on the wall, "
     puts "#{pluralize(num_bottles, 'bottle')} of beer."
     num_bottles -= 1
     print 'Take one down and pass it around, '
-    puts "#{pluralize(num_bottles, 'bottle')} of beer on the wall."
-    puts ''
+    puts "#{pluralize(num_bottles, 'bottle')} of beer on the wall.\n\n"
     song(num_bottles)
   else
-    puts 'No more bottles of beer on the wall, no more bottles of beer.'
-    puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+    ending
   end
 end
 
