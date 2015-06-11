37f86de9b5a4a34d08bd358d74ba7887ad790350
Rebecca <rfjackson11@gmail.com>
2015-06-11 13:23:54 -0600

    Add pluralize method
    [#96593178]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,30 +1,24 @@
 #!/usr/bin/env ruby
 
+def pluralize(number, string)
+  if number > 1
+    "#{number} #{string}s"
+  elsif number == 0
+    'no more bottles'
+  else
+    "#{number} #{string}"
+  end
+end
+
 def song(num_bottles)
-  if num_bottles > 2
-    print "#{num_bottles} bottles of beer on the wall, "
-    puts "#{num_bottles} bottles of beer."
+  if num_bottles > 0
+    print "#{pluralize(num_bottles, 'bottle')} of beer on the wall, "
+    puts "#{pluralize(num_bottles, 'bottle')} of beer."
     num_bottles -= 1
     print 'Take one down and pass it around, '
-    puts "#{num_bottles} bottles of beer on the wall."
+    puts "#{pluralize(num_bottles, 'bottle')} of beer on the wall."
     puts ''
     song(num_bottles)
-  elsif num_bottles == 2
-    print "#{num_bottles} bottles of beer on the wall, "
-    puts "#{num_bottles} bottles of beer."
-    num_bottles -= 1
-    print 'Take one down and pass it around, '
-    puts "#{num_bottles} bottle of beer on the wall."
-    puts ''
-    song(num_bottles)
-  elsif num_bottles == 1
-    print "#{num_bottles} bottle of beer on the wall, "
-    puts "#{num_bottles} bottle of beer."
-    num_bottles -= 1
-    print 'Take one down and pass it around, '
-    puts 'no more bottles of beer on the wall.'
-    puts ''
-    puts song(num_bottles)
   else
     puts 'No more bottles of beer on the wall, no more bottles of beer.'
     puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
