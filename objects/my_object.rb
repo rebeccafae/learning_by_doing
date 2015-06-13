cbffb939ce3090212c45f303f1a08ac11b98e1d6
Rebecca <rfjackson11@gmail.com>
2015-06-13 15:22:31 -0600

    Finish CoffeeMaker object
    [#96738802]

--- objects/my_object.rb
+++ objects/my_object.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/env ruby
 
+# Check to see if your coffee maker is ready with .ready?
+# See what you're missing with .display_errors
+# Add water, filter, or coffee_grounds with .add_water(how_much?) (etc)
+# Brew your coffee with .brew
+# If you try to brew when it's not ready it will tell you that it's not ready
 class CoffeeMaker
   def initialize
     @water = 0
@@ -19,31 +24,53 @@ class CoffeeMaker
     @grounds = amount
   end
 
-  def prepare
-    if @water > 0 && @filter == true && @grounds >=0
-      @ready = 'ready'
+  def ready?
+    if water? && filter? && coffee_grounds?
+      true
     else
-      'Did you add water, coffee grounds, and a filter?'
+      false
     end
   end
 
+  def errors
+    error_array = []
+    error_array << 'You need to add water.' unless water?
+    error_array << 'You are missing a filter.' unless filter?
+    error_array << 'You need to add some coffee grounds.' unless coffee_grounds?
+    error_array
+  end
+
+  def display_errors
+    errors.each { |e| puts e }
+  end
+
+  def water?
+    @water > 0
+  end
+
+  def coffee_grounds?
+    @grounds > 0
+  end
+
+  def filter?
+    @filter
+  end
+
   def brew
-    if @ready == 'ready'
+    if ready?
       @water = 0
       @grounds = 0
       @filter = false
       puts 'Your coffee is brewing... Enjoy!'
-    else @ready != 'ready'
+    else
       puts 'Your coffee maker is not ready.'
     end
   end
-  # add methods to grind beans, turn on, and off
 end
 
 coffee = CoffeeMaker.new
 puts coffee.add_water(2)
 puts coffee.add_coffee_grounds(2)
 puts coffee.add_filter
-puts coffee.prepare
-puts coffee.brew
+coffee.ready?
 puts coffee.brew
