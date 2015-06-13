a0cc47fc3f6cc4a386f4883594c35ab603208e43
Rebecca <rfjackson11@gmail.com>
2015-06-13 13:55:42 -0600

    Change lines 10 and 15
    [#96733212]

--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -7,13 +7,12 @@ puts 'T-' + numbers_array.reverse.join(', ') + '...  BLASTOFF!'
 puts "The last element is #{numbers_array.last}"
 puts "The first element is #{numbers_array.first}"
 puts "The third element is #{numbers_array[2]}"
-print 'The element with an index of '
-puts "#{numbers_array.index(4)} is #{numbers_array[3]}"
+puts "The element with an index of 3 is #{numbers_array[3]}"
 puts "The second from last element is #{numbers_array[-2]}"
 puts "The first four elements are '#{numbers_array.first(4).join(', ')}'"
 print "If we delete #{numbers_array.delete(5)}, #{numbers_array.delete(6)} "
 print "and #{numbers_array.delete(7)} from the array, "
-puts "we're left with #{numbers_array.to_s.gsub(' ', '')}"
+puts "we're left with [#{numbers_array.join(',')}]"
 print "If we add #{numbers_array.unshift(5).first} at the beginning of "
 # same array output as line 16 but trying it a different way
 puts "the array, we're left with [#{numbers_array.join(',')}]"
