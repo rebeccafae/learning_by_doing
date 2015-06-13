2052eba21a10ccd1caa2ef8d2900933c20f97078
Rebecca <rfjackson11@gmail.com>
2015-06-12 18:29:27 -0600

    Update coffee maker
    [#96738802]

--- objects/my_object.rb
+++ objects/my_object.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
 
 class CoffeeMaker
+  def initialize
+    @water = 0
+    @grounds = 0
+    @filter = false
+  end
+
   def add_water(number_of_cups)
     @water = number_of_cups
   end
@@ -15,14 +21,29 @@ class CoffeeMaker
 
   def prepare
     if @water > 0 && @filter == true && @grounds >=0
-      @ready = true
+      @ready = 'ready'
     else
       'Did you add water, coffee grounds, and a filter?'
     end
   end
 
   def brew
-    @ready == true ? 'Your coffee is brewing' : 'Your coffee maker is not ready.'
+    if @ready == 'ready'
+      @water = 0
+      @grounds = 0
+      @filter = false
+      puts 'Your coffee is brewing... Enjoy!'
+    else @ready != 'ready'
+      puts 'Your coffee maker is not ready.'
+    end
   end
   # add methods to grind beans, turn on, and off
 end
+
+coffee = CoffeeMaker.new
+puts coffee.add_water(2)
+puts coffee.add_coffee_grounds(2)
+puts coffee.add_filter
+puts coffee.prepare
+puts coffee.brew
+puts coffee.brew
