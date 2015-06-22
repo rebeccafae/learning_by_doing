abb8299514bb0b7e741a30014d007d18b95ab901
Rebecca <rfjackson11@gmail.com>
2015-06-21 19:48:56 -0600

    Finished CashRegister class and spec
    [#97444924]

--- objects_tdd/lib/cash_register.rb
+++ objects_tdd/lib/cash_register.rb
@@ -15,7 +15,6 @@ class CashRegister
     if change <= 0
       @total = change.abs.round(2)
       puts "Your change is $#{@total}"
-      # @total = 0.00
     else
       @total = change
       puts "Your new total is $#{@total}"
--- objects_tdd/spec/lib/cash_register_spec.rb
+++ objects_tdd/spec/lib/cash_register_spec.rb
@@ -9,19 +9,12 @@ describe CashRegister do
     it 'should return 0.00 for initial total' do
       expect(subject.total).to eq(0.00)
     end
-
-    # it 'should go back to 0.00 after giving change' do
-    #   subject.purchase(3.78)
-    #   subject.pay(5.00)
-    #   subject.total
-    #   expect(subject.total).to eq(0.00)
-    # end
   end
 
   describe '#purchase(price)' do
     it 'should add the price to the total' do
-      subject.purchase(3.00)
-      expect(subject.total).to eq(3.00)
+      subject.purchase(10.00)
+      expect(subject.total).to eq(10.00)
     end
   end
 
@@ -33,15 +26,15 @@ describe CashRegister do
     end
 
     it 'should give the amount of change' do
-      subject.purchase(3.78)
-      subject.pay(5.00)
-      expect(subject.total).to eq(1.22)
+      subject.purchase(5.00)
+      subject.pay(10.00)
+      expect(subject.total).to eq(5.00)
     end
 
     it 'should give the new total if not paid in full' do
       subject.purchase(10)
-      subject.pay(6)
-      expect(subject.total).to eq(4)
+      subject.pay(5)
+      expect(subject.total).to eq(5)
     end
   end
 end
